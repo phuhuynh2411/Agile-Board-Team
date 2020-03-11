31f6467da6d1480e6f2dc67cfb80e9157bf2d176
Phu Huynh <phu@filesoft.tech>
2020-03-11 08:53:21 +0700

    Initial Commit

---
+++ Agile Board Team/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Agile Board Team
+//
+//  Created by Huynh Tan Phu on 3/11/20.
+//  Copyright © 2020 Filesoft. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
